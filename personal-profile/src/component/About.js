dd4c2a75408c31baf2b5281733b8bd8192198695
cjustine09 <delacruzcarljustine92@gmail.com>
2024-10-20 11:01:58 +0800

    update

--- personal-profile/src/component/About.js
+++ personal-profile/src/component/About.js
@@ -5,7 +5,10 @@ const About = () => {
   return (
     <Container className="mt-5">
       <h2>About Me</h2>
-      <p>I enjoy coding and have a background in software development. I’ve worked on multiple projects using React, JavaScript, and more.</p>
+      <p>About me I'm interest in coding and practice my self to imorove and expant my knowledge
+        to communicate others to gain more experience for coding, other me I love being alone to 
+        peace of my mind, cooking one my love doing when i free, also i play mobolie game
+      </p>
     </Container>
   );
 };
